ad3a4bd9001f7f9a140ba0dc8ab82234213263b8
Mike <theshadowmoose@gmail.com>
2020-06-01 18:54:07 -0700

    Update build.py

--- build.py
+++ build.py
@@ -66,6 +66,7 @@ for o in weapons:
 				o['totalDamage'] = round(dmg)
 				o['fireRate'] = rate
 				o['damagePerSecond'] = round(rate*dmg)
+				o['magazineSize'] = o['magazineSize'] if 'magazineSize' in o else 0
 				break
 
 ordered = sorted(weapons, key=lambda x: (x['disposition'], x['damagePerSecond'], len(x['name'])), reverse=True)
