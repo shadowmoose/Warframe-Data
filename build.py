628425f210368c1ee4b38c8ca43873e674a3df01
ShadowMoose <theshadowmoose@gmail.com>
2020-07-17 16:40:51 -0700

    Fix missing values.

--- build.py
+++ build.py
@@ -17,8 +17,10 @@ bkup = requests.get(backup).text
 bkup = bkup.split('WeaponData =')[1].split('return WeaponData')[0].strip()
 backup = {}
 
+
 def get(obj, tag, def_val=0):
 	return obj[tag] if tag in obj else def_val
+
 
 for k, v in lua.decode(bkup)['Weapons'].items():
 	key = html.unescape(k).lower()
@@ -26,7 +28,7 @@ for k, v in lua.decode(bkup)['Weapons'].items():
 
 print('Parsing data...')
 
-weapons = [i for i in data if 'disposition' in i and 'type' in i]
+weapons = [i for i in data if 'disposition' in i and 'type' in i and get(i, 'type') != 'Exalted Weapon']
 
 stats = {}
 
@@ -44,27 +46,25 @@ print('Scanned unique stats:')
 for s in stats:
 	print(s, end=' - ')
 	print(all(s in w for w in weapons))
-
+print('\nBuilding...\n')
 
 for o in weapons:
-	if get(o, 'type', '') == 'Exalted Weapon':
-		continue
 	# Some of the API data is missing valid DPS numbers, so correct them using the wiki.
 	if get(o, 'damagePerSecond', 0) <= 1 or get(o, 'totalDamage', 0) <= 20:
-		if o['name'].lower() not in backup.keys():
-			raise Exception('No backup found: %s' % o['name'])
+		if get(o, 'name', '').lower() not in backup.keys():
+			raise Exception('No backup found: %s' % get(o, 'name', ''))
 		else:
-			bk = backup[o['name'].lower()]
+			bk = backup[get(o, 'name', '').lower()]
 			dmg = 0
 			for at in ['NormalAttack', 'AreaAttack', 'ChargeAttack']:
 				if at not in bk:
 					continue
-				print(o['name'], bk[at])
+				# print(get(o, 'name', ''), bk[at])
 				dmg = 0
 				for k, v in bk[at]['Damage'].items():
 					dmg += v
 				dmg = round(dmg, 2)
-				rate = o['fireRate']
+				rate = get(o, 'fireRate')
 				if 'FireRate' in bk[at]:
 					rate = bk[at]['FireRate']
 				o['totalDamage'] = round(dmg)
@@ -72,7 +72,7 @@ for o in weapons:
 				o['damagePerSecond'] = round(rate*dmg)
 				break
 
-ordered = sorted(weapons, key=lambda x: (x['disposition'], x['damagePerSecond'], len(x['name'])), reverse=True)
+ordered = sorted(weapons, key=lambda x: (get(x, 'disposition', 0), get(x, 'damagePerSecond', 0), len(x['name'])), reverse=True)
 
 with open('./data/Rivens.md', 'w') as out:
 	out.write('# Riven Dispositions\n\n')
@@ -80,8 +80,16 @@ with open('./data/Rivens.md', 'w') as out:
 		out.write('## Disposition %s\n\n' % x)
 		out.write('''|Weapon | Type | Total DPS | Trigger | Hit Damage | Fire Rate| Magazine|\n--- | --- | --- | --- | --- | --- | ---\n''')
 		for o in filter(lambda y: y['disposition'] == x, ordered):
-			out.write('[%s](%s)|%s|%s|%s|%s|%s|%s\n' %
-					  (o['name'], o['wikiaUrl'], o['type'] or 'Unknown', round(o['damagePerSecond']), get(o, 'trigger', '-'), round(o['totalDamage'], 2), round(o['fireRate'], 2), get(o, 'magazineSize', '-')) )
+			out.write('[%s](%s)|%s|%s|%s|%s|%s|%s\n' % (
+						o['name'],
+						o['wikiaUrl'],
+						get(o, 'type', None) or 'Unknown',
+						round(get(o, 'damagePerSecond')),
+						get(o, 'trigger', '-'),
+						round(get(o, 'totalDamage'), 2),
+						round(get(o, 'fireRate'), 2),
+						get(o, 'magazineSize', '-')
+			))
 		out.write('\n\n')
 	out.write('__Updated:__ %s UTC' % datetime.now(timezone.utc).strftime("%Y-%m-%d, %H:%M:%S"))
 
